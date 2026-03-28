667f0ca7bac206c900883ac00db1914b89d1f5bb
Ashton Pomerantz <ashtonpomerantz@gmail.com>
2026-03-28 14:38:46 -0700

    corrected json post output

--- backend/recommend.go
+++ backend/recommend.go
@@ -6,25 +6,29 @@ import (
 )
 
 
-func recommendMeals(meals []Meal, filters Filters) []RecResult {
-	results := make([]RecResult, 0)
+func recommendMeals(meals []Meal, filters Filters) []Meal {
+	results := make([]Meal, 0)
 
 	for _, meal := range meals {
 		if !passesEdgeCases(meal, filters) {
 			continue
 		}
 
-		results = append(results, RecResult{
-			ID:												meal.ID,
-			Search:										meal.Search,
-			EstimatedCostPerServing:	meal.EstimatedCostPerServing,
-			TotalCookTimeMinutes:			meal.TotalCookTimeMinutes,
-			Dietary:									meal.Tags.Dietary,
-			Constraints:							meal.Tags.Constraints,
-			Mood:											meal.Tags.Mood,
-			Cuisine:									meal.Tags.Cuisine,
-			Description:							meal.Description,
-		})
+		results = append(results, meal)
+
+		// results = append(results, RecResult{
+		// 	ID:												meal.ID,
+		// 	Name:											meal.Name,
+		// 	EstimatedCostPerServing:	meal.EstimatedCostPerServing,
+		// 	TotalCookTimeMinutes:			meal.TotalCookTimeMinutes,
+		// 	Ingredients:							meal.Ingredients,
+		// 	Tags:											meal.Tags,
+		// 	// Dietary:									meal.Tags.Dietary,
+		// 	// Constraints:							meal.Tags.Constraints,
+		// 	// Mood:											meal.Tags.Mood,
+		// 	// Cuisine:									meal.Tags.Cuisine,
+		// 	Description:							meal.Description,
+		// })
 
 		if len(results) > 5 {
 			results = results[:5]
@@ -39,7 +43,36 @@ func recommendMeals(meals []Meal, filters Filters) []RecResult {
 // statement helpers
 func passesEdgeCases(meal Meal, filters Filters) bool {
 	// return false for edge case failures
-	// TODO: edge cases
+	// budget edge case
+	if filters.Budget > 0 && meal.EstimatedCostPerServing > filters.Budget {
+		return false
+	}
+
+	// time edge case
+	if filters.TimeConstraint > 0 && meal.TotalCookTimeMinutes > filters.TimeConstraint {
+		return false
+	}
+
+	// dietary edge case
+	for _, tag := range filters.Dietary {
+		found := false
+		for _, mealTag := range meal.Tags.Dietary {
+			if mealTag == tag {
+				found = true
+				break
+			}
+		}
+		if !found {
+			return false
+		}
+	}
+
+	// constraints edge case
+
+	// mood edge case
+
+	// cuisine edge case
+
 
 	// else return true
 	return true
